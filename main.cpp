b133ddb0b49f96d19d1a1e9e2bc3c9c295f2c712
NamrathaV99 <53892356+NamrathaV99@users.noreply.github.com>
2019-08-10 21:00:16 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+
 int main()
 {
-	 std::fstream myfile("C:/Users/lenovo/Desktop/ADA LAB/Additional_Assignment/Input.txt", std::ios_base::in);
-	int n,i,j,a[100],k,t,flag=-1;
-    cout<<"Enter the no. of test cases:"<<endl;
-	myfile>>t;
+    int t,n,k,a[20],flag=0;
+    ifstream inFile;
+      inFile.open("Input.txt");
+    if (!inFile) {
+        cout << "Unable to open file";
+         // terminate with error
+    }
+    inFile >> t;
+    cout<<"No. of test cases : "<<t<<endl;
+    for(int i=0;i<t;i++)
+    {
+        flag=0;
+        inFile >> n;
+        cout<<"The no. of elements : "<<n<<endl;
+        inFile >> k;
+        cout<<"The key : "<<k<<endl;
+        cout<<"The elements :"<<endl;
+        for (int i=0;i<n;i++)
+        {
+            inFile >>a[i];
+            cout<<a[i]<<" , ";
+            if(a[i]==k)
+                flag=1;
+        }
 
+        if(flag==1)
+            cout<<endl<<1<<" KEY FOUND ";
+        else
+            cout<<endl<<-1<<" KEY NOT FOUND " ;
+       cout<<endl;
+    }
 
-	cout<<"Enter the number of array elements and the number to be found "<<endl;
-
-	for(i=0;i<t;i++)
-	{
-		myfile>>n;
-	    myfile>>k;
-
-		cout<<"Enter the array elements "<<endl;
-		for(j=0;j<n;j++)
-		{
-			myfile>>a[j];
-			if(a[j] == k)
-				flag=1;
-		}
-
-		if(flag==1)
-			cout<<"Element found!!!"<<endl;
-		else
-			cout<<"Not found";
-
-	}
-
+    inFile.close();
+    return 0;
 }
