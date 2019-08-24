5f152ec20eb41e777f11d65e0dc405fa8e8cdf7a
NamrathaV99 <53892356+NamrathaV99@users.noreply.github.com>
2019-08-24 10:27:12 +0530

    Add files via upload

---
+++ kth_smallest_selectionsort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+void swap(int *x, int *y)
+{
+	int temp=*x;
+	*x=*y;
+	*y=temp;
+}
+
+int kthsmallest(int a[],int n, int k)
+{   int min;
+	for(int i=0;i<k;i++)
+	{
+		min =i;
+		for(int j=i+1;j<=n-1;j++)
+		{
+			if(a[j]<a[min])
+				min=j;
+		}
+		swap(a[i],a[min]);
+	}
+	return a[k-1];
+}
+
+int main(int argc, char **argv)
+{
+	int n,a[50],k,x;
+	cout<<"Enter the no. of elements in the array : ";
+	cin>>n;
+	cout<<"Enter the elements of the array :"<<endl;
+	for (int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	cout<<"Enter the value of k :";
+	cin>>k;
+	x=kthsmallest(a,n,k);
+	cout<<"Kth smallest element in the array = "<<x<<endl;
+	return 0;
+}
